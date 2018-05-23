8ec3bb396ad3c545e6e1fe706e8a05032de1b13d
Leila Allen <leilaallen@students.berkeley.net>
2018-05-23 09:38:19 -0700

    dfsddee

--- studySets.js
+++ studySets.js
@@ -3,7 +3,7 @@ $(function() {
     for(var i in studySets) {
         console.log(studySets[i]);
         var save = $("#allSets").html();
-        $("#allSets").html(save + "<div value='" + i + "' class='studySets'>" + studySets[i][0] + "  <div class='studySets'>" + studySets[i][1] +"</div></div>");
+        $("#allSets").html(save + "<div value='" + i + "' class='studySets'>" + studySets[i][0] + "<br><a href=" + studySets[i][2] + "class='studySets'>" + studySets[i][1] +"</a></div>");
         $("#studySets").click(function() {
 
         });
@@ -20,5 +20,5 @@ $(function() {
     })
 });
 function getCollection() {
-    return [["study set 1","preview", "link"], ["study set 2","preview23dknf", "link"]];
+    return [["study set 1","preview", "https://www.miniclip.com/games/agar-io/en/#t-n-G"], ["study set 2","preview23dknf", "https://www.miniclip.com/games/diepio/en/#t-404-U"]];
 }
